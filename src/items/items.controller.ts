583e03ba78928bc638196513514498bc7a9e6967
Lucas Brandi <lgbrandidantas17@gmail.com>
2026-04-25 20:16:47 -0300

    finalizando a aula com  a criação e modularização do mongodb

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -6,38 +6,53 @@ import {
   Delete,
   Body,
   Param,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interface/item.interface';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
   @Get()
-  findAll(): string {
-    return 'all items';
+  findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id): string {
-    return `Item ${id}`;
+  async findOne(@Param('id') id): Promise<Item> {
+    const item = await this.itemsService.findOne(id);
+    if (!item) {
+      throw new NotFoundException(`Item with ID ${id} not found`);
+    }
+    return item;
   }
 
   @Post()
-  create(@Body() createItemDto: CreateItemDto): string {
-    return `Created item: ${createItemDto.name}`;
-  }
-
-  @Put()
-  update(): string {
-    return 'update item';
-  }
-
-  @Delete()
-  delete(): string {
-    return 'delete item';
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto);
   }
 
   @Delete(':id')
-  deleteOne(@Param('id') id): string {
-    return `Deleted item ${id}`;
+  async delete(@Param('id') id): Promise<Item> {
+    const item = await this.itemsService.delete(id);
+    if (!item) {
+      throw new NotFoundException(`Item with ID ${id} not found`);
+    }
+    return item;
+  }
+
+  @Put(':id')
+  async update(
+    @Body() updateItemDto: CreateItemDto,
+    @Param('id') id,
+  ): Promise<Item> {
+    const item = await this.itemsService.update(id, updateItemDto);
+    if (!item) {
+      throw new NotFoundException(`Item with ID ${id} not found`);
+    }
+    return item;
   }
 }
